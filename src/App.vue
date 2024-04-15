1df2a9aa79300de7806b56a2cd03aac4be456035
lv1codeman <lv1codeman@gmail.com>
2024-04-16 01:28:09 +0800

    安裝ant-design-vue，啟用router

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import { DownCircleOutlined } from "@ant-design/icons-vue";
+import { ref, toRef } from "vue";
+
+const items = ref([
+  "Button 1",
+  "Button 2",
+  "Button 3",
+  "Button 4",
+  "Button 5",
+  "Button 6",
+  "Button 7",
+]);
+const size = "small";
+</script>
+
+<template>
+  <a-button v-for="item in items" :key="item">{{ item }}</a-button>
+
+  <div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped></style>
